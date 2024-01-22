0f8b33809fbd17ce833cedf9aa63a1b48d92d658
ivabalasix2324 <ivabalasix2324@proton.me>
2024-01-22 16:52:35 +0000

    Commit 1.3.1 de l'aplicacio ipcalc

--- app/index.php
+++ app/index.php
@@ -8,10 +8,10 @@
 	</head>
 	<body>
 		<p><b>CALCUALDORA IPv4</b></p>
+		<p>Autor: asix2 - gp1f4a11 - ver 1.3<p>
+		<?php
+                echo date("l jS \of F Y h:i:s A") . "<br>";
+                ?>
 		<a href="ip.html">Aplicació de càlcul de subxarxes</a>
-		<p>Autor: asix2</p>
 	</body>
 <html>
-<?php
-	echo date("l jS \of F Y h:i:s A") . "<br>";
-?>
